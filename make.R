da6f2c72a983929d2d4ea1851375197efd3112f4
Johannes Brachem <jobrachem@posteo.de>
2019-09-08 22:23:56 +0200

    make test

--- make.R
+++ make.R
@@ -8,6 +8,13 @@ source("code/n_participants.R")
 source("code/prepare_data.R")
 source("code/wrangling.R")
 
+# analysis files
+rmarkdown::render("code/sample_descriptives.Rmd", knit_root_dir = "..", 
+                  output_file = "../docs/sample_descriptives.html")
+
+rmarkdown::render("code/analysis.Rmd", knit_root_dir = "..", 
+                  output_file = "../docs/analysis.html")
+
 # plots
 source("code/plots/attitudes.R")
 source("code/plots/n_qrps_per_project.R")
@@ -18,20 +25,17 @@ source("code/plots/stats_software.R")
 source("code/plots/study_progress.R")
 
 
-# analysis files
-rmarkdown::render("code/sample_descriptives.Rmd", knit_root_dir = "..", 
-                  output_file = "../docs/sample_descriptives.html")
 
-rmarkdown::render("code/analysis.Rmd", knit_root_dir = "..", 
-                  output_file = "../docs/analysis.html")
 
 # source("code/exploration.R")
 
 # render report
-rmarkdown::render("manuscript/manuscript-jo2.Rmd", knit_root_dir = "..", 
-                  output_file = "../manuscript/manuscript.docx")
+# knit directly from file
+# rmarkdown::render("manuscript/manuscript.Rmd", knit_root_dir = "..", 
+#                   output_file = "../manuscript/manuscript.docx")
 
 # render online supplements
-rmarkdown::render("manuscript/supplementary_data.Rmd", knit_root_dir = "..", 
+# knit directly from file
+rmarkdown::render("manuscript/supplements.Rmd", knit_root_dir = "..", 
                   output_file = "../manuscript/supplements.docx")
 
